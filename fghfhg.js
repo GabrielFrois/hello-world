e3213b8acc8e1eb30a41d7a83e52ce1e0c9a7dae
joaoestreano <joaovictorestreano@gmail.com>
2024-09-25 21:22:58 -0300

    Update fghfhg.js

--- fghfhg.js
+++ fghfhg.js
@@ -1,2 +1,16 @@
 console.log('a');
 i = 0;
+function inverte(a){
+    invertida='';
+    i=a.length-1
+    while(i>=0) {
+        invertida += a[i];
+        i--;
+    }
+    return invertida;
+}
+
+r=inverte("pedro")
+console.log("invertido:", r)
+r=inverte("Mariana")
+console.log("invertido:", r)
